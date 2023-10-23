191f430e9f4b266dd8dd03356d49bde0e2c8ba38
TT2M <120509171+TT2M@users.noreply.github.com>
2023-10-23 20:27:20 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle('Главная');
+?> 
+<?
+echo'КОНТЕНТ';?>
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
